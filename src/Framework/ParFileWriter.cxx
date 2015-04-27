10a6ba86808c3d829664984e193f023120c06192
Andrea Lani <andrea.lani@gmail.com>
2015-04-27 15:14:42 +0200

    AL: Added missing files.

---
+++ src/Framework/ParFileWriter.cxx
@@ -0,0 +1,115 @@
+// Copyright (C) 2012 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#include "Framework/ParFileWriter.hh"
+#include "Common/PE.hh"
+#include "Common/CFPrintContainer.hh"
+
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace COOLFluiD::Common;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace COOLFluiD {
+
+  namespace Framework {
+
+//////////////////////////////////////////////////////////////////////////////
+
+ParFileWriter::ParFileWriter() : 
+  FileWriter(),
+  _comm(0),
+  _fh(),
+  _status(),
+  _myRank(0),
+  _nbProc(0),
+  _ioRank(0),
+  _myGroupID(0),
+  _offset(),
+  _isNewFile(false),
+  _isWriterRank(false),
+  _fileList(),
+  _mapFileToStartNodeList()
+{
+  _comm   = PE::GetPE().GetCommunicator();
+  _myRank = PE::GetPE().GetRank();
+  _nbProc = PE::GetPE().GetProcessorCount();
+  cf_assert(_nbProc > 0);
+  
+  _nbWriters = 1;
+  _maxBuffSize = 2147479200;
+}
+    
+//////////////////////////////////////////////////////////////////////////////
+
+ParFileWriter::~ParFileWriter()
+{
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void ParFileWriter::setWriterGroup()
+{
+  CFAUTOTRACE;
+  
+  if (_nbWriters > _nbProc) {
+    CFLog(WARN, "ParFileWriter::setWriterGroup() => _nbWriters > _nbProc, therefore they are set equal!\n");
+    _nbWriters = _nbProc;
+  }
+  
+  // in reality ranks will be user-defined 
+  vector<int> ranks;
+  vector<int> writerRanks(_nbWriters);
+  CFint count = 0;
+  bool first = true;
+  for (CFuint i = 0; i < _nbWriters; ++i) {    
+    const CFuint nbProcPerWriter = _nbProc/_nbWriters;
+    const CFuint nranks =  (i < _nbWriters-1) ? nbProcPerWriter : nbProcPerWriter + _nbProc%_nbWriters;
+    ranks.resize(nranks);
+    for (CFuint r = 0; r < nranks; ++r, ++count) { 
+      ranks[r] = count;
+      if (ranks[r] == _myRank) {
+	// store the ID of the corresponding group
+	_myGroupID = i;
+	cf_assert(first); // sanity check to be sure that each rank is associated to one group
+	first = false;
+      }
+    }
+    // PE::createGroup(ranks, true);
+    
+    // the first rank in each group will be the master process
+    writerRanks[i] = ranks[0];
+    if (ranks[0] == _myRank) {
+      _isWriterRank = true;
+    }
+  }
+  
+  // create the writers group
+  PE::createGroup("Writers",writerRanks, false);
+  
+  CFLog(VERBOSE, "ParFileWriter::setWriterGroup() => " << 
+	CFPrintContainer<vector<int> >(" writerRanks  = ",  &writerRanks) << "\n");  
+  
+  // if (_myRank == 0) {
+  //   for (CFuint i = 0; i < _nbProc; ++i) {
+  //     cout << "rank [" << i << "] in group " << PE::getGroupName(i) << endl;
+  //   }
+  // }
+  
+  // create an info object
+  // MPI_Info_create(&_info);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+  } // namespace Framework
+
+} // namespace COOLFluiD
+
+//////////////////////////////////////////////////////////////////////////////
+
---
+++ src/Framework/ParFileWriter.hh
@@ -0,0 +1,117 @@
+// Copyright (C) 2012 von Karman Institute for Fluid Dynamics, Belgium
+//
+// This software is distributed under the terms of the
+// GNU Lesser General Public License version 3 (LGPLv3).
+// See doc/lgpl.txt and doc/gpl.txt for the license text.
+
+#ifndef COOLFluiD_Framework_ParFileWriter_hh
+#define COOLFluiD_Framework_ParFileWriter_hh
+
+//////////////////////////////////////////////////////////////////////////////
+
+#include <mpi.h>
+#include <set>
+
+#include "Framework/FileWriter.hh"
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace COOLFluiD {
+
+  namespace Framework {
+
+//////////////////////////////////////////////////////////////////////////////
+
+/// This class provides a basic interface for parallel file writers
+/// @author Andrea Lani
+class Framework_API ParFileWriter : public FileWriter {
+public:
+  
+  /// Default constructor without arguments
+  ParFileWriter();
+  
+  /// Default destructor
+  virtual ~ParFileWriter();
+  
+  /// Gets the file extension to append to the file name
+  virtual const std::string getWriterFileExtension() const = 0;
+  
+ protected:
+  
+  /// Get the name of the writer
+  virtual const std::string getWriterName() const = 0;
+  
+  /// Set group of writers
+  virtual void setWriterGroup();  
+  
+ protected: //data
+  
+  /// Class holding all offsets defining the parallel file structure
+  class Offset {
+  public:
+    /// start/end of the element list
+    std::pair<MPI_Offset, MPI_Offset> elems;
+    
+    /// start/end of the nodes list
+    std::pair<MPI_Offset, MPI_Offset> nodes;
+    
+    /// start/end of the states list
+    std::pair<MPI_Offset, MPI_Offset> states;
+    
+    /// start/end of the TRS element list
+    std::vector<std::pair<MPI_Offset, MPI_Offset> > TRS;
+  };
+  
+  /// communicator
+  MPI_Comm _comm;
+  
+  /// file handler
+  MPI_File _fh;
+  
+  /// file status
+  MPI_Status _status;
+    
+  /// rank of this processor
+  CFuint _myRank;
+  
+  /// number of processors
+  CFuint _nbProc;
+  
+  /// I/O rank
+  CFuint _ioRank;
+  
+  /// group ID
+  CFuint _myGroupID;
+  
+  /// offsets holder
+  Offset _offset;
+    
+  /// flag telling if the file is a new one
+  bool _isNewFile;
+
+  /// flag telling if this processor is a writer
+  bool _isWriterRank;
+  
+  /// set keeping track of the files already created
+  std::set<boost::filesystem::path> _fileList;
+  
+  /// set keeping track of the files already created
+  std::map<boost::filesystem::path, long long> _mapFileToStartNodeList;
+  
+  /// number of writers (and MPI groups)
+  CFuint _nbWriters;
+  
+  /// maximu size of the buffer to write with MPI I/O
+  int _maxBuffSize;
+  
+}; // end of class ParFileWriter
+
+//////////////////////////////////////////////////////////////////////////////
+
+  } // namespace Framework
+
+} // namespace COOLFluiD
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif // COOLFluiD_Framework_ParFileWriter_hh
