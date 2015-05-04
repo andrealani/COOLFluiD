6976136295fccbc81582e700b8dd198d9001ae9b
MilheiroSantos <milheiro.santos@gmail.com>
2015-05-04 18:09:58 +0200

    Mesh fitting algorithm

--- plugins/FiniteVolume/MeshFittingAlgorithm.hh
+++ plugins/FiniteVolume/MeshFittingAlgorithm.hh
@@ -4,10 +4,10 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #include "Environment/SingleBehaviorFactory.hh"
-#include "Environment/FileHandlerOutput.hh"
 #include "Framework/DataProcessingData.hh"
 #include "Framework/DataSocketSink.hh"
 #include "Framework/CellTrsGeoBuilder.hh"
+#include "SimpleEdgeGraph.hh"
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -24,11 +24,22 @@ namespace COOLFluiD {
       
 //////////////////////////////////////////////////////////////////////////////
 
+//Storage of truncation data for the springs
+struct SpringTrucationData{
+  CFreal minLimit;
+  CFreal maxLimit;
+  CFreal mean;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+
+
 /**
  *
  * This class implements a shock-fitting mesh algorithm to
  * redistribute mesh points close to shocks
  *
+ * @author Pedro Santos
  * @author Francisco Moreira Huhn
  *
  */
@@ -65,7 +76,7 @@ public:
   virtual void unsetup();
 
   /**
-   * Execute on a set of dofs
+   * Executes the mesh fitting
    */
   virtual void execute();
 
@@ -89,29 +100,34 @@ public:
 private:
   
   /**
-   * Creates the "note to node" and "node to cell" connectivity
+   * Creates the nodal connectivity
    */
-  void createConnectivity();
+  void createNodalConnectivity();
 
   /**
-   * Determine and register non-modifiable nodes
+   * Determine and register locked nodes
    */
-  void determineNonModNodes();
+  void determineLockedNodes();
   
   /**
-   * Compute gradient of the monitor variable Phi
+   * Compute nodal gradient of the monitor variable
    */
-  void computeGradientPhi();
+  RealVector computeNodalGradient(const Framework::Node* node);
   
   /**
    * Compute the spring constants for each edge
    */
-  void computeSprings();
-  
+  void computeSpringTruncationData();
+ 
   /**
-   * Fill the matrix and RHS of the system to be solved
+   * truncates the spring constant
    */
-  void fillSystem();
+  CFreal truncateSpringConstant(CFreal springConstant); 
+
+  /**
+   * Solve Linear System
+   */
+  void solveLinearSystem();
   
   /**
    * Update nodes with new positions
@@ -119,38 +135,32 @@ private:
   void updateNodePositions();
   
   /**
-   * Synchronize nodes and recompute mesh data (normals, volumes, etc.)
+   * Send message to trigger re-computation of mesh information
    */
-  void syncNodesAndRecomputeMeshData();
+  void triggerRecomputeMeshData();
   
-  /**
-   * Run the shock sensor on the monitor variable Phi
-   */
-  void shockSensor();
-  
-  /**
-   * Compute Euclidean distance between two Nodes
-   */
-  CFreal getDistance(Framework::Node& a, Framework::Node& b);
+ /**
+  * Queries if the node is locked 
+  */
+  bool isNodeLocked(const Framework::Node* node);
 
-  bool isNodeLocked(Framework::Node* node);
+  /**
+   * fills the rhs of the linear system
+   */ 
+  void fillSpringsRightHandSide();
 
-  
-CFreal computeSpringConstant(const Framework::Node* const firstNode, const Framework::Node* const secondNode) ;
+  /**
+   * fills the coefficient matrix of the linear system
+   */
+  void fillSpringsMatrix();
+
+  /**
+   * computes the spring constant between two nodes
+   */
+  CFreal computeSpringConstant(const Framework::Node* const firstNode, 
+                               const Framework::Node* const secondNode) ;
 
 private: //data
-
-  /// storage of shockSensor values
-  Framework::DataSocketSource<CFreal> socket_shockSensor;
-  
-  /// storage of the gradient of the monitor variable Phi
-  Framework::DataSocketSource<CFreal> socket_gradientPhi;
-  
-  /// storage of the pressure
-  Framework::DataSocketSource<CFreal> socket_pressure;
-  
-  /// storage of volumes
-  Framework::DataSocketSink<CFreal> socket_volumes;
 
   /// the socket to the data handle of the state's
   Framework::DataSocketSink < Framework::Node* , Framework::GLOBAL > socket_nodes;
@@ -164,14 +174,8 @@ private: //data
   /// the socket to the data handle of the nodal state's
   Framework::DataSocketSink<Framework::State*> socket_gstates;
   
-  /// storage of the face normals
-  Framework::DataSocketSink<CFreal> socket_normals;
-  
   /// storage of the RHS
   Framework::DataSocketSink<CFreal> socket_rhs;
-  
-  /// storage of the flag for IDs of cells for which normals are outward
-  Framework::DataSocketSink<CFint> socket_isOutward;
   
   /// pointer to the linear system solver
   Common::SafePtr<Framework::LinearSystemSolver> m_lss;
@@ -182,77 +186,32 @@ private: //data
   /// builder of geometric entities
   Framework::GeometricEntityPool<Framework::CellTrsGeoBuilder> m_geoBuilder;
   
-  /// Euler physical data
-  RealVector m_physicalData;
-  
   // accumulator for LSSMatrix
   // std::auto_ptr<Framework::BlockAccumulator> m_acc;
   
-  /// minimum pressure ratio for shock sensor
-  CFreal m_phiMinSS;
-  
-  /// maximum pressure ratio for shock sensor
-  CFreal m_phiMaxSS;
-  
   /// Minimum percentile limit for spring constants
-  CFreal m_minPerc;
+  CFreal m_minPercentile;
 
   /// Maximum percentile limit for spring constants
-  CFreal m_maxPerc;
+  CFreal m_maxPercentile;
   
   /// Step for mesh adaptation
-  CFreal m_alpha;
+  CFreal m_meshAcceleration;
   
-  /// Shock sensor rate
-  CFuint m_ssRate;
-  
-  /// Mesh adaptation rate
-  CFuint m_maRate;
-
   /// Monitor variable string
   std::string m_monitorVar;
   
-  /// Monitor variable (nothing to do with Shock Sensor phi)
-  CFuint m_phi;
-
-  /// Degree from boundary for non-modifiable nodes
-  CFuint m_boundaryDegreeNM;
-  
-  /// Stop adaptation but keep shock sensor
-  CFuint m_stopAdaptationIter;
-  
-  /// Connectivity "node to node"
-  /// connectedNodes[i] gives a set whose members
-  /// are localIDs of nodes connected to node i
-  std::vector< std::set<CFuint> > m_connectedNodes;
-
-  /// Connectivity "node to cell"
-  /// connectedCells[i] gives a set whose members
-  /// are localIDs of states adjacent to node i
-  std::vector< std::set<CFuint> > connectedCells;
-
-  /// Connectivity "node to cell"
-  /// connectedCells[i] gives a set whose members
-  /// are localIDs of gstates adjacent to node i  
-  std::vector< std::set<CFuint> > connectedGhosts;
-
-  /// Connectivity "gstate to node"
-  /// ghostNodes[i] gives a vector whose members
-  /// are localIDs of nodes adjacent to gstate i
-  std::vector< std::vector<CFuint> > ghostNodes;
+  /// Monitor variable ID)
+  CFuint m_monitorVarID;
 
   /// Set of Boundary Nodes
   std::set<CFuint> m_boundaryNodes;
+
+  //Storage for the truncation data of the springs
+  SpringTrucationData m_springTruncationData;
     
-  /// Set of Non-Modifiable Nodes
-  std::set<CFuint> m_nonModifiableNodes;
-
-  /// Visited array for boundary BFS
-  std::vector<bool> visited;
-
-  CFreal m_minLim;
-  CFreal m_maxLim;
-  CFreal m_avgKe;
+  //Simple edge graph 
+  FiniteVolume::SimpleEdgeGraph m_edgeGraph;
 
 }; // end of class MeshFittingAlgorithm
 
