609ee1a99bce1e7372272964568cf828637f3788
Andrea Lani <andrea.lani@gmail.com>
2015-10-29 20:24:43 +0100

    AL: Added examples of new user-defined constants and functions for the function parser.

--- src/MathTools/FunctionParser.hh
+++ src/MathTools/FunctionParser.hh
@@ -34,6 +34,44 @@ class MathTools_API FunctionParser_cd: public FunctionParserBase<std::complex<do
 class MathTools_API FunctionParser_cf: public FunctionParserBase<std::complex<float> > {};
 class MathTools_API FunctionParser_cld: public FunctionParserBase<std::complex<long double> > {};
 
+//----------------------------------------------------------------------------//
+
+/// New functions to be added to the default ones 
+
+/// radius in 2D
+static double Radius2D(const double* x)
+{
+  return std::sqrt(x[0]*x[0] + x[1]*x[1]);
+} 
+
+/// radius in 3D
+static double Radius3D(const double* x)
+{
+  return std::sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
+}
+    
+//----------------------------------------------------------------------------//
+
+// This class derives from the FunctionParser defined in the library from 
+/// @see website http://warp.povusers.org/FunctionParser/
+// DerivedParser adds some constants and functions to the set already available 
+// in the original library
+
+// @author Andrea Lani
+
+class DerivedParser: public FunctionParser
+{
+public:
+  DerivedParser() : FunctionParser()
+  {
+    AddConstant("pi", 3.14159265358979323846);
+    AddConstant("e", 2.71828182845904523536);
+    AddConstant("Rair", 287.046);
+    AddFunction("R2", Radius2D, 2);
+    AddFunction("R3", Radius3D, 3);
+  }
+};
+
 //////////////////////////////////////////////////////////////////////////////
 
   } // namespace MathTools
