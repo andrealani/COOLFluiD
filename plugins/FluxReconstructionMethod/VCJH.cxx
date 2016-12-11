5c05543abfe1366154595cf1da7cccc972903468
AlexanderPapen <alexanderpapen@hotmail.com>
2016-12-11 17:52:33 +0100

    AP: Added computeCorrectionFunction1D

---
+++ plugins/FluxReconstructionMethod/VCJH.cxx
@@ -0,0 +1,126 @@
+#include <iterator>
+
+#include "Framework/MethodStrategyProvider.hh"
+
+#include "FluxReconstructionMethod/FluxReconstructionElementData.hh"
+#include "FluxReconstructionMethod/FluxReconstruction.hh"
+#include "FluxReconstructionMethod/VCJH.hh"
+
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace COOLFluiD::Framework;
+using namespace COOLFluiD::Common;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace COOLFluiD {
+
+  namespace FluxReconstructionMethod {
+
+//////////////////////////////////////////////////////////////////////////////
+
+Framework::MethodStrategyProvider<
+    VCJH,FluxReconstructionSolverData,BaseCorrectionFunction,FluxReconstructionModule >
+VCJHProvider("VCJH");
+
+//////////////////////////////////////////////////////////////////////////////
+
+VCJH::VCJH(const std::string& name) :
+  BaseCorrectionFunction(name)
+{
+  CFAUTOTRACE;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+VCJH::~VCJH()
+{
+  CFAUTOTRACE;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+      
+void VCJH::computeCorrectionFunction(FluxReconstructionElementData frElemData, std::vector< std::vector< RealVector > > corrcts)
+{
+    CFAUTOTRACE;
+    const CFGeoShape::Type elemShape = frElemData.getShape();
+    switch(elemShape)
+    {
+      case CFGeoShape::QUAD:
+      {
+      const CFuint nbrSolPnts1D = frElemData.getSolPntsLocalCoord1D()->size();
+      for (CFuint iKsi = 0; iKsi < nbrSolPnts1D; ++iKsi)
+      {
+          for (CFuint iEta = 0; iEta < nbrSolPnts1D; ++iEta)
+          {
+              
+          }
+      }
+      } break;
+      default:
+      {
+        throw Common::NotImplementedException (FromHere(),"VCJH Correction Functions not implemented for elements of type "
+                                               + StringOps::to_str(elemShape) + ".");
+      }
+    }
+}
+      
+//////////////////////////////////////////////////////////////////////////////
+      
+CFreal VCJH::computeCorrectionFunction1D(CFPolyOrder::Type solOrder, CFreal ksi, CFreal cfactor)
+{
+    CFAUTOTRACE;
+    CFreal corrfct;
+    switch(solOrder)
+    {
+        case CFPolyOrder::ORDER1:
+        {
+            corrfct = -0.5*(ksi-(1.5*cfactor+0.5*(3.*ksi*ksi-1.))/(1.+22.5*cfactor));
+        } break;
+        case CFPolyOrder::ORDER2:
+        {
+            corrfct = 0.5*(0.5*(3.*ksi*ksi-1.)-(22.5*cfactor*ksi+0.5*(5.*ksi*ksi*ksi-3.*ksi))/(1.+22.5*cfactor));
+        } break;
+        case CFPolyOrder::ORDER3:
+        {
+            corrfct = -0.5*(0.5*(5.*ksi*ksi*ksi-3.*ksi)-(787.5*cfactor*0.5*(3.*ksi*ksi-1.)+0.125*(35.*ksi*ksi*ksi*ksi-30.*ksi*ksi+3.))/(1.+787.5*cfactor));
+        } break;
+        case CFPolyOrder::ORDER4:
+        {
+            corrfct = 0.5*(0.125*(35.*ksi*ksi*ksi*ksi-30.*ksi*ksi+3.)-(49612.5*cfactor*0.5*(5.*ksi*ksi*ksi-3.*ksi)+0.125*(63.*ksi*ksi*ksi*ksi*ksi-70.*ksi*ksi*ksi+15.*ksi))/(1.+49612.5*cfactor));
+        } break;
+        case CFPolyOrder::ORDER5:
+        {
+            corrfct = -0.5*(0.125*(63.*ksi*ksi*ksi*ksi*ksi-70.*ksi*ksi*ksi+15.*ksi)-(4911637.5*cfactor*0.125*(35.*ksi*ksi*ksi*ksi-30.*ksi*ksi+3.)+0.0625*(231.*ksi*ksi*ksi*ksi*ksi*ksi-315.*ksi*ksi*ksi*ksi+105.*ksi*ksi-5.))/(1.+4911637.5*cfactor));
+        } break;
+        default:
+        {
+            throw Common::NotImplementedException (FromHere(),"VCJH1D Correction Function not implemented for order " + StringOps::to_str(solOrder) + ".");
+        }
+    }
+    return corrfct;
+}
+      
+//////////////////////////////////////////////////////////////////////////////
+
+void VCJH::setup()
+{
+  CFAUTOTRACE;
+
+  BaseCorrectionFunction::setup();
+
+}
+
+void VCJH::unsetup()
+{
+  CFAUTOTRACE;
+
+  BaseCorrectionFunction::unsetup();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+  }  // namespace FluxReconstructionMethod
+
+}  // namespace COOLFluiD
--- plugins/FluxReconstructionMethod/VCJH.hh
+++ plugins/FluxReconstructionMethod/VCJH.hh
@@ -5,7 +5,7 @@
 
 #include "Framework/BaseMethodStrategyProvider.hh"
 #include "FluxReconstructionMethod/FluxReconstructionSolverData.hh"
-#include "FluxReconstructionMethod/RiemannFlux.hh"
+#include "FluxReconstructionMethod/BaseCorrectionFunction.hh"
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -21,31 +21,28 @@ namespace COOLFluiD {
  * @author Ray Vandenhoeck
  * @author Alexander Papen
  */
-class CentredFlux : public RiemannFlux {
+class VCJH : public BaseCorrectionFunction {
 
 public:  // methods
 
   /// Constructor
-  CentredFlux(const std::string& name);
+  VCJH(const std::string& name);
 
   /// Destructor
-  ~CentredFlux();
-
-  /// Compute the Riemann flux, from left and right states and a normal vector
-  virtual RealVector& computeFlux(Framework::State& lState, RealVector& lExtraVars,
-                                  Framework::State& rState, RealVector& rExtraVars,
-                                  const RealVector& normal);
-                                  
-  /// Compute the Riemann flux, from left and right states and a normal vector
-  virtual RealVector& computeFlux(Framework::State& lState,
-                                  Framework::State& rState,
-                                  const RealVector& normal);
-
+  ~VCJH();
+    
+  /// Compute the VCJH correction function of an instance of FluxReconstructionElementData
+  void computeCorrectionFunction(FluxReconstructionElementData frElemData, const std::vector< std::vector< RealVector > > corrcts);
+    
+    
+private :
+  /// Compute the value of the VCJH 1D correction function of order p at ksi
+  CFreal computeCorrectionFunction1D(CFPolyOrder::Type solOrder, CFreal ksi, CFreal cfactor);
 
   /// Gets the Class name
   static std::string getClassName()
   {
-    return "CentredFlux";
+    return "VCJH";
   }
 
   /// Set up private data and data
@@ -54,12 +51,8 @@ public:  // methods
   /// Unset up private data and data
   virtual void unsetup();
 
-private: // data
 
-  /// array storing the sum of the right and left flux
-  RealVector  m_sumFlux;
-
-}; // class CentredFlux
+}; // class VCJH
 
 //////////////////////////////////////////////////////////////////////////////
 
