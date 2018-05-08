c91b78310e33b1eec21502079a2fa4bde35140a8
Andrea Lani <andrea.lani@gmail.com>
2018-05-08 18:34:05 +0200

    AL: Implemented new inhomogenous Neumann condition that extrapolates boundary value from file.

---
+++ plugins/FiniteVolume/NeumannBCFromFile.cxx
@@ -0,0 +1,131 @@
+#include "FiniteVolume/FiniteVolume.hh"
+#include "FiniteVolume/NeumannBCFromFile.hh"
+#include "Framework/MethodCommandProvider.hh"
+#include "Framework/MapGeoToTrsAndIdx.hh"
+
+//////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace COOLFluiD::Framework;
+using namespace COOLFluiD::Common;
+using namespace COOLFluiD::Config;
+using namespace COOLFluiD::MathTools;
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace COOLFluiD {
+
+  namespace Numerics {
+
+    namespace FiniteVolume {
+
+//////////////////////////////////////////////////////////////////////////////
+
+MethodCommandProvider<NeumannBCFromFile, CellCenterFVMData, FiniteVolumeModule> 
+neumannBCFromFileFVMCCProvider("NeumannBCFromFileFVMCC");
+      
+//////////////////////////////////////////////////////////////////////////////
+
+void NeumannBCFromFile::defineConfigOptions(Config::OptionList& options)
+{
+  options.addConfigOption< vector<CFuint> >
+    ("VarIDs","IDs of the variables from which values are read by file");
+}
+      
+//////////////////////////////////////////////////////////////////////////////
+
+NeumannBCFromFile::NeumannBCFromFile(const std::string& name) :
+  SuperInlet(name)
+{
+  addConfigOptionsTo(this);
+  
+  m_varIDs = vector<CFuint>();
+  setParameter("VarIDs",&m_varIDs);
+}
+      
+//////////////////////////////////////////////////////////////////////////////
+
+NeumannBCFromFile::~NeumannBCFromFile()
+{
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void NeumannBCFromFile::setup()
+{
+  SuperInlet::setup();
+  
+  if (m_varIDs.size() == 0) {m_varIDs.resize(1, 0);}
+  
+  m_mapGeoToTrs =
+    MeshDataStack::getActive()->getMapGeoToTrs("MapFacesToTrs");
+  
+  // build the m_mapTrs2Twall storage
+  vector< SafePtr<TopologicalRegionSet> >& trsList = this->getTrsList();
+  
+  for (CFuint iTrs = 0; iTrs < trsList.size(); ++iTrs) {
+    SafePtr<TopologicalRegionSet> trs = trsList[iTrs];
+    const CFuint nbTrsFaces = trs->getLocalNbGeoEnts();
+    RealVector* tWall = new RealVector(0.0,nbTrsFaces);
+    m_mapTrs2Twall.insert(&*trs, tWall);
+  }
+  m_mapTrs2Twall.sortKeys();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void NeumannBCFromFile::unsetup()
+{
+  SuperInlet::unsetup();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+void NeumannBCFromFile::setGhostState(GeometricEntity *const face)
+{
+  State *const innerState = face->getState(0);
+  State *const ghostState = face->getState(1);
+
+  cf_assert(getMethodData().getNodalStatesExtrapolator()->getMapTrs2NodalValues()->size() > 0);
+  
+  vector<Node*>& nodesInFace = *face->getNodes();
+  const CFuint nbNodesInFace = nodesInFace.size();
+  SafePtr<TopologicalRegionSet> trs = m_mapGeoToTrs->getTrs(face->getID());
+  
+  // build the mapTrs2NodalValues storage
+  SafePtr<NodalStatesExtrapolator<CellCenterFVMData> > nse =
+    this->getMethodData().getNodalStatesExtrapolator();
+  SafePtr<vector<NodalStatesExtrapolator<CellCenterFVMData>::MapTrs2NodalValues*> >
+    mapTrs2NodalValues = nse->getMapTrs2NodalValues();
+
+  for (CFuint iVar = 0; iVar < m_varIDs.size(); ++iVar) {
+    const CFuint varID = m_varIDs[iVar];
+    RealVector& bArray = *(*mapTrs2NodalValues)[varID]->find(&*trs);
+    CFMap<CFuint,CFuint>* mapNodeIDs = nse->getMapTrs2NodeIDs()->find(&*trs);
+    
+    // compute the temperature in the face centroid as the average of the nodal values
+    (*_input)[varID] = 0.0;
+    for (CFuint iNode = 0; iNode < nbNodesInFace; ++iNode) {
+      const CFuint localNodeID = nodesInFace[iNode]->getLocalID();
+      (*_input)[varID] += bArray[mapNodeIDs->find(localNodeID)];
+    }
+    (*_input)[varID] /= nbNodesInFace; 
+  }
+  
+  // we assume that (U_i - U_g)/dr = f(x,y,z) => U_g = U_i - f*dr
+  const CFreal dr = MathTools::MathFunctions::getDistance
+    (ghostState->getCoordinates(), innerState->getCoordinates());
+  
+  CFLog(DEBUG_MED, "NeumannBCFromFile::setGhostState() => (*_input) = " << *_input << ", dr = " << dr  << "\n");
+  *ghostState = *innerState - (*_input)*dr;
+  
+  CFLog(DEBUG_MED, "NeumannBCFromFile::setGhostState() => ghostState = " << *ghostState << "\n");
+}
+      
+//////////////////////////////////////////////////////////////////////////////
+
+    } // namespace FiniteVolume
+
+  } // namespace Numerics
+
+} // namespace COOLFluiD
---
+++ plugins/FiniteVolume/NeumannBCFromFile.hh
@@ -0,0 +1,89 @@
+#ifndef COOLFluiD_Numerics_FiniteVolume_NeumannBCFromFile_hh
+#define COOLFluiD_Numerics_FiniteVolume_NeumannBCFromFile_hh
+
+//////////////////////////////////////////////////////////////////////////////
+
+#include "Framework/NormalsCalculator.hh"
+#include "Framework/VolumeCalculator.hh"
+#include "FiniteVolume/SuperInlet.hh"
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace COOLFluiD {
+
+  namespace Framework {
+    class MapGeoToTrsAndIdx;
+  }
+  
+  namespace Numerics {
+
+    namespace FiniteVolume {
+
+//////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * This class represents a Neumann boundary condition
+   *
+   * @author Andrea Lani
+   *
+   */
+class NeumannBCFromFile : public SuperInlet {
+public:
+
+  /**
+   * Defines the Config Option's of this class
+   * @param options a OptionList where to add the Option's
+   */
+  static void defineConfigOptions(Config::OptionList& options);
+
+  /**
+   * Constructor
+   */
+  NeumannBCFromFile(const std::string& name);
+
+  /**
+   * Default destructor
+   */
+  virtual ~NeumannBCFromFile();
+  
+  /**
+   * Set up private data and data of the aggregated classes
+   * in this command before processing phase
+   */
+  virtual void setup();
+
+  /**
+   * Unset up private data and data of the aggregated classes
+   * in this command after processing phase
+   */
+  virtual void unsetup();
+
+  /**
+   * Apply boundary condition on the given face
+   */
+  virtual void setGhostState(Framework::GeometricEntity *const face);
+
+protected:
+  
+  /// pointer to the mapping face - TRS
+  Common::SafePtr<Framework::MapGeoToTrsAndIdx> m_mapGeoToTrs;
+  
+  /// map each wall faceID to the corresponding wall temperature
+  Common::CFMap<Framework::TopologicalRegionSet*, RealVector*> m_mapTrs2Twall;
+
+  /// IDs of the variables from which values are read by file
+  std::vector<CFuint> m_varIDs;
+  
+}; // end of class NeumannBCFromFile
+
+//////////////////////////////////////////////////////////////////////////////
+
+ } // namespace FiniteVolume
+
+  } // namespace Numerics
+
+} // namespace COOLFluiD
+
+//////////////////////////////////////////////////////////////////////////////
+
+#endif // COOLFluiD_Numerics_FiniteVolume_NeumannBCFromFile_hh
