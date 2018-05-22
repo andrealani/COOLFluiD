40fdb176a3a068ca4b2b73ede300428294172a81
Andrea Lani <andrea.lani@gmail.com>
2018-05-22 13:58:12 +0200

    AL: playing around with the testcase, still failing

--- plugins/FiniteVolume/NeumannBCFromFile.cxx
+++ plugins/FiniteVolume/NeumannBCFromFile.cxx
@@ -91,25 +91,32 @@ void NeumannBCFromFile::setGhostState(GeometricEntity *const face)
   vector<Node*>& nodesInFace = *face->getNodes();
   const CFuint nbNodesInFace = nodesInFace.size();
   SafePtr<TopologicalRegionSet> trs = m_mapGeoToTrs->getTrs(face->getID());
+  cf_assert(trs.isNotNull());
   
   // build the mapTrs2NodalValues storage
   SafePtr<NodalStatesExtrapolator<CellCenterFVMData> > nse =
     this->getMethodData().getNodalStatesExtrapolator();
   SafePtr<vector<NodalStatesExtrapolator<CellCenterFVMData>::MapTrs2NodalValues*> >
     mapTrs2NodalValues = nse->getMapTrs2NodalValues();
-
+  
+  cf_assert(m_varIDs.size() > 0);
+  
   for (CFuint iVar = 0; iVar < m_varIDs.size(); ++iVar) {
     const CFuint varID = m_varIDs[iVar];
+    cf_assert(varID < mapTrs2NodalValues->size());
+
     RealVector& bArray = *(*mapTrs2NodalValues)[varID]->find(&*trs);
     CFMap<CFuint,CFuint>* mapNodeIDs = nse->getMapTrs2NodeIDs()->find(&*trs);
     
     // compute the temperature in the face centroid as the average of the nodal values
+    cf_assert(varID < _input->size());
+    
     (*_input)[varID] = 0.0;
     for (CFuint iNode = 0; iNode < nbNodesInFace; ++iNode) {
       const CFuint localNodeID = nodesInFace[iNode]->getLocalID();
       (*_input)[varID] += bArray[mapNodeIDs->find(localNodeID)];
     }
-    (*_input)[varID] /= nbNodesInFace; 
+    (*_input)[varID] /= nbNodesInFace;
   }
   
   // we assume that (U_i - U_g)/dr = f(x,y,z) => U_g = U_i - f*dr
