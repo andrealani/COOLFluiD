f702e1c17fc74fa3a39ff1c380253e23fe1eb1c1
RayVandenhoeck <ray.vandenhoeck@gmail.com>
2019-11-09 10:06:35 +0100

    RV: generalized BDF time RHS of FR for different variable sets

--- plugins/FluxReconstructionMethod/BDF2TimeRHSJacob.cxx
+++ plugins/FluxReconstructionMethod/BDF2TimeRHSJacob.cxx
@@ -4,6 +4,8 @@
 #include "Framework/LSSMatrix.hh"
 #include "Framework/MeshData.hh"
 #include "Framework/SubSystemStatus.hh"
+#include "Framework/BlockAccumulator.hh"
+
 
 #include "FluxReconstructionMethod/FluxReconstruction.hh"
 #include "FluxReconstructionMethod/FluxReconstructionElementData.hh"
@@ -68,28 +70,60 @@ void BDF2TimeRHSJacob::addTimeResidual()
 
   // add time residual contribution (Backward Euler)
   const CFuint nbrSolPnts = m_cellStates->size();
+  
+  for (CFuint iSol = 0; iSol < nbrSolPnts; ++iSol)
+  {
+    // now the contribution to the jacobian matrix is calculated
+    m_acc->setRowColIndex(iSol, (*m_cellStates)[iSol]->getLocalID());
+  }
+  
   for (CFuint iSol = 0; iSol < nbrSolPnts; ++iSol)
   {
     // get state
-    const State& currState = *(*m_cellStates)[iSol];
+    State *const currState = (*m_cellStates)[iSol];
+    
+   m_tempState = static_cast<RealVector&>(*m_updateToSolutionVecTrans->transform(currState));
 
     // get state ID
     const CFuint stateID = (*m_cellStates)[iSol]->getLocalID();
 
     // get past state
-    const State& pastState = *pastStates[stateID];
+    const State *const pastState = m_updateToSolutionVecTrans->transform(pastStates[stateID]); //*pastStates[stateID];
 
     // add contribution to rhs and jacobian
     CFuint globalID = idxMapping.getColID(stateID)*m_nbrEqs;
     for (CFuint iEq = 0; iEq < m_nbrEqs; ++iEq, ++globalID)
     {
-      const CFreal dUdt = (currState[iEq] - pastState[iEq])*m_diagValues[iSol];
+      const CFreal dUdt = (m_tempState[iEq] - (*pastState)[iEq])*m_diagValues[iSol];
       rhs(stateID,iEq,m_nbrEqs) -= dUdt*resFactorP1 - resFactor*pastTimeRhs(stateID,iEq,m_nbrEqs);
-
-      if (getMethodData().doComputeJacobian())
+      
+      if((!getMethodData().isSysMatrixFrozen()) && getMethodData().doComputeJacobian())
       {
-        m_jacobMatrix->addValue(globalID, globalID, m_diagValues[iSol]*resFactorP1);
+        // perturb the given component of the state vector
+        m_numericalJacob->perturb(iEq, (*currState)[iEq]);
+
+        const RealVector& tempPertState = static_cast<RealVector&>(*m_updateToSolutionVecTrans->transform(currState));
+
+        // compute the finite difference derivative of the flux
+        m_numericalJacob->computeDerivative(m_tempState,tempPertState,m_fluxDiff);
+
+        // _fluxDiff corresponds to a column vector of the dU/dP matrix
+        for (CFuint iEq = 0; iEq < m_nbrEqs; ++iEq) 
+        {
+	  m_fluxDiff[iEq] *= m_diagValues[iSol]*resFactorP1;
+        }
+
+        m_acc->addValues(iSol, iSol, iEq, &m_fluxDiff[0]);
+
+        // restore the unperturbed value
+        m_numericalJacob->restore((*currState)[iEq]);
+
       }
+
+//      if (getMethodData().doComputeJacobian())
+//      {
+//        m_jacobMatrix->addValue(globalID, globalID, m_diagValues[iSol]*resFactorP1);
+//      }
 
       // store dUdt for next time step
       if(SubSystemStatusStack::getActive()->isLastStep())
@@ -97,6 +131,13 @@ void BDF2TimeRHSJacob::addTimeResidual()
         pastTimeRhs(stateID,iEq,m_nbrEqs) = dUdt;
       }
     }
+    
+    // add the values in the jacobian matrix
+    //getMethodData().getLSSMatrix(0)->addValues(*m_acc);
+    m_lss->getMatrix()->addValues(*m_acc);
+
+    // reset to zero the entries in the block accumulator
+    m_acc->reset();
   }
 }
 
