49ede7eae2301e0d422a4b0b7f9d0b92756a0979
Andrea Lani <andrea.lani@gmail.com>
2021-02-12 16:03:45 +0100

    AL: added providers

--- plugins/FiniteVolumeMHD/HLLEFluxMHD.cxx
+++ plugins/FiniteVolumeMHD/HLLEFluxMHD.cxx
@@ -1,6 +1,7 @@
 #include "FiniteVolumeMHD/HLLETanakaFlux.hh"
 #include "FiniteVolumeMHD/FiniteVolumeMHD.hh"
 #include "Framework/MethodStrategyProvider.hh"
+#include "MHD/MHD2DProjectionVarSet.hh"
 #include "MHD/MHD3DProjectionVarSet.hh"
 
 //////////////////////////////////////////////////////////////////////////////
@@ -24,6 +25,18 @@ MethodStrategyProvider<HLLETanakaFlux<MHD3DProjectionVarSet>,
                        FluxSplitter<CellCenterFVMData>,
                        FiniteVolumeMHDModule>
 hlleTanakaMHD3DFluxSplitterProvider("HLLETanakaMHD3D");
+
+MethodStrategyProvider<HLLEFlux<MHD2DProjectionVarSet>,
+                       CellCenterFVMData,
+                       FluxSplitter<CellCenterFVMData>,
+                       FiniteVolumeMHDModule>
+hlleMHD2DFluxSplitterProvider("HLLEMHD2D");
+
+MethodStrategyProvider<HLLEFlux<MHD3DProjectionVarSet>,
+                       CellCenterFVMData,
+                       FluxSplitter<CellCenterFVMData>,
+                       FiniteVolumeMHDModule>
+hlleMHD3DFluxSplitterProvider("HLLEMHD3D");
       
 //////////////////////////////////////////////////////////////////////////////
 
