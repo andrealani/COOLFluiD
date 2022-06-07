d80801d7317fc742c7c16bcc0190b3624e37ea5e
Andrea Lani <andrea.lani@gmail.com>
2022-06-07 16:31:38 +0200

    AL: changed headers

--- plugins/FluxReconstructionMethod/FluxReconstructionSolverData.cxx
+++ plugins/FluxReconstructionMethod/FluxReconstructionSolverData.cxx
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 von Karman Institute for Fluid Dynamics, Belgium
+// Copyright (C) 2016 KU Leuven, von Karman Institute for Fluid Dynamics, Belgium
 //
 // This software is distributed under the terms of the
 // GNU Lesser General Public License version 3 (LGPLv3).
